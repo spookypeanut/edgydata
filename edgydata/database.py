d6215378dcb45d4c144e270f82d88b63eb2f7a6c
spookypeanut <github@spookypeanut.co.uk>
2017-06-13 13:53:18 +0100

    Raw sql of db creation for reference, to be removed

--- edgydata/database.py
+++ edgydata/database.py
@@ -12,3 +12,44 @@ class LocalDatabase(object):
             path = get_db_path()
         self._conn = sqlite3.connect(path)
         self._cursor = self._conn.cursor()
+"""
+CREATE TABLE purchased (
+    time time,
+        value float,
+            site_id integer
+            );
+
+            CREATE TABLE consumption (
+                    time time,
+                    value float,
+                    site_id integer
+            );
+
+            CREATE TABLE self_consumption (
+                    time time,
+                    value float,
+                    site_id integer
+            );
+
+            CREATE TABLE production (
+                    time time,
+                    value float,
+                    site_id integer
+            );
+
+            CREATE TABLE feed_in (
+                    time time,
+                    value float,
+                    site_id binary
+            );
+
+            CREATE TABLE sites (
+                    id integer,
+                    name string,
+                    peak_power float,
+                    installation_date date,
+                    last_update_time time,
+                    lifetime_energy float
+            );
+
+"""
