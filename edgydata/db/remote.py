714e2bc6f825eb7e41a9d0d99c9fe87d549b81f4
spookypeanut <github@spookypeanut.co.uk>
2017-10-28 22:19:43 +0100

    Ability to get site ids

--- edgydata/db/remote.py
+++ edgydata/db/remote.py
@@ -1,14 +1,44 @@
+import os
+import requests
+from copy import deepcopy
+
 from edgydata.data import Site
 
+BASE_URL = "https://monitoringapi.solaredge.com"
+
+
+class ResponseError(IOError):
+    """ The response from SolarEdge was not in the form we expected """
+
+
 class Remote(object):
-    def __init__(self, api_key):
-        self._api_key = api_key
+    def __init__(self, api_key=None):
+        if api_key is None:
+            try:
+                self._api_key = os.environ["SOLAREDGEAPI"]
+            except KeyError:
+                raise IOError("Cannot find a SolarEdge API key")
+        else:
+            self._api_key = api_key
+
+    def _remote_call(self, sub_url, data):
+        url = "%s/%s" % (BASE_URL, sub_url)
+        data_with_api = deepcopy(data)
+        data_with_api.update({"api_key": self._api_key})
+        response = requests.get(url, params=data_with_api)
+        if not response.ok:
+            print(response.content)
+            raise ResponseError("API call failed: %s" % response.reason)
+        return response.json()
 
     def _get_site_ids(self):
         """ Get all the site ids of all the sites connected with this SolarEdge
         account
         """
-        pass
+        data = self._remote_call("sites/list", {})
+        ids = [a["id"] for a in data["sites"]["site"]]
+        # Return this as a list, in the same order as the API gives them
+        return ids
 
     def get_sites(self):
         """ Return Site objects for all the sites connected with this SolarEdge
