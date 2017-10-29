39577ed3b6a7f8067bec9dba91a92b5693c64b11
spookypeanut <github@spookypeanut.co.uk>
2017-10-29 07:20:04 +0000

    Implement get_site

--- edgydata/db/remote.py
+++ edgydata/db/remote.py
@@ -1,11 +1,18 @@
 import os
 import requests
+from datetime import datetime
 from copy import deepcopy
 
 from edgydata.data import Site
 
 BASE_URL = "https://monitoringapi.solaredge.com"
+DATE_FORMAT = "%Y-%m-%d"
 
+def _date_to_string(input_date):
+    return input_date.strftime(DATE_FORMAT)
+
+def _date_from_string(input_string):
+    return datetime.strptime(input_string, DATE_FORMAT).date()
 
 class ResponseError(IOError):
     """ The response from SolarEdge was not in the form we expected """
@@ -53,4 +60,13 @@ class Remote(object):
         """ Return a Site object from a given site id, by querying the details
         from the SolarEdge API
         """
-        pass
+        sub_url = "site/%s/details.json" % site_id
+        result = self._remote_call(sub_url)
+        raw = result["details"]
+        raw_start = raw["installationDate"]
+        raw_end = raw["lastUpdateTime"]
+        kwargs = {"site_id": site_id,
+                  "start_date": _date_from_string(raw_start),
+                  "end_date": _date_from_string(raw_end),
+                  "peak_power": raw["peakPower"]}
+        return Site(**kwargs)
