95d380f16037bcd60b9cba48c7d1daa637dfd9a0
spookypeanut <github@spookypeanut.co.uk>
2017-10-29 07:19:50 +0000

    We don't have lifetime energy in the details, so skip it

--- edgydata/data.py
+++ edgydata/data.py
@@ -1,16 +1,13 @@
 class Site(object):
     """ An object that represents one of the solar generation sites returned by
-    the SolarEdge API. While the peak_power and lifetime_energy can be given,
-    they don't have to be (since they can be deduced from the rest of the
-    data).
+    the SolarEdge API. While peak_power can be given, it doesn't have to be
+    (since it can sort of be deduced from the rest of the data).
     """
-    def __init__(self, site_id, start_date, end_date,
-                 peak_power=0, lifetime_energy=0):
+    def __init__(self, site_id, start_date, end_date, peak_power=0):
         self.site_id = site_id
         self.start_date = start_date
         self.end_date = end_date
         self.peak_power = peak_power
-        self.lifetime_energy = lifetime_energy
 
 
 class Usage(object):
