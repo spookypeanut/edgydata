a8a52648dac76d6a71fdcf77dd0d10c324c222b8
spookypeanut <github@spookypeanut.co.uk>
2017-11-04 23:14:27 +0000

    Add ability to get attrs and types

--- edgydata/data.py
+++ edgydata/data.py
@@ -1,14 +1,30 @@
+from datetime import date
+
+
 class Site(object):
     """ An object that represents one of the solar generation sites returned by
     the SolarEdge API. While peak_power can be given, it doesn't have to be
     (since it can sort of be deduced from the rest of the data).
     """
+    # If you add an attribute, make sure you add it here...
+    _types = {"site_id": int, "name": str, "start_date": date, "end_date": date,
+              "peak_power": float}
+
     def __init__(self, site_id, name, start_date, end_date, peak_power=0):
+        # ... and here
         self.site_id = site_id
         self.name = name
         self.start_date = start_date
         self.end_date = end_date
         self.peak_power = peak_power
+
+    @classmethod
+    def list_attrs(cls):
+        return sorted(cls._types.keys())
+
+    @classmethod
+    def attr_types(cls):
+        return cls._types
 
 
 class Usage(object):
