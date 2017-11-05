55081d9547ef50290240cd511a6024618bde0fcf
spookypeanut <github@spookypeanut.co.uk>
2017-11-05 21:33:09 +0000

    Docstrings and comments

--- edgydata/db/local.py
+++ edgydata/db/local.py
@@ -10,40 +10,56 @@ _TYPE_LOOKUP = {str: "STRING", int: "INTEGER", float: "FLOAT",
 
 
 def _date_to_int(mydate):
+    """ Convert a datetime.date object to a unix timestamp """
     return _datetime_to_int(datetime.combine(mydate, datetime.min.time()))
 
 
 def _datetime_to_int(mytime):
+    """ Convert a datetime.datetime object to a unix timestamp. """
+    # Apparently, this is the safest way to do it (ignoring timezones)
     return int((mytime - datetime(1970, 1, 1)).total_seconds())
 
 
 def _int_to_datetime(myint):
+    """ Convert a unix timestamp to a datetime.datetime object """
     return datetime.fromtimestamp(myint)
 
 
 def _int_to_date(myint):
+    """ Convert a unix timestamp to a datetime.date object """
     return _int_to_datetime(myint).date()
 
-
+# The converters to use to put object types into and get them out of the
+# database. First element is to put them in, second to get them out
 _CONVERTER = {date: (_date_to_int, _int_to_date),
               datetime: (_datetime_to_int, _int_to_datetime)}
 
 
 def _check(mystr):
+    """ Ensure a string isn't trying to inject any dodgy SQL """
+    # Although the input strings are all self-generated atm, this could
+    # change in future
     if mystr != mystr.translate(None, ")(][;,"):
         raise RuntimeError("Input '%s' looks dodgy to me" % mystr)
     return mystr
 
 
 def get_db_path():
+    """ Get the file path to the sqlite database """
     return os.path.join(os.environ["HOME"], "edgydata.db")
 
 
 class Local(AbstractDB):
+    """ A local mirror of the solar power data, stored in a sqlite database.
+    The intention is that the extraction API is the same as
+    `edgydata.db.remote.Remote`, but there isn't any benefit to using
+    inheritance here.
+    """
     site_table = "site"
     power_table = "power"
 
     def __init__(self, path=None):
+        # If we get given a path, use it, but we can make up our own
         if path is None:
             self._dbpath = get_db_path()
         else:
@@ -52,6 +68,7 @@ class Local(AbstractDB):
         self._cursor = self._conn.cursor()
 
     def _execute(self, sql, variables=None):
+        """ Execute an sql query, after optionally printing it """
         print("Executing:")
         print(sql)
         if variables is None:
