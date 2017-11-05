d654cd2d6cfd831584f46bf9dd9d7049c287e97d
spookypeanut <github@spookypeanut.co.uk>
2017-11-05 21:31:46 +0000

    Add an abstract db
    Just debugging stuff atm

---
+++ edgydata/db/abstract.py
@@ -0,0 +1,15 @@
+import time
+
+
+class AbstractDB(object):
+    """ An abstract class that the remote, local and hybrid dbs can inherit
+    from. This doesn't contain a lot as there is little overlap between all
+    three.
+    """
+    def __init__(self, debug=True):
+        self._debug = debug
+
+    def debug(self, msg):
+        if not self._debug:
+            return
+        print("[%s] %s" % (time.asctime(), msg))
