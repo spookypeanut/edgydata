7406ca7f7fcb70807e210d08153bc70b6d482caf
spookypeanut <github@spookypeanut.co.uk>
2018-07-22 13:25:01 +0100

    Enum constants

--- edgydata/constants.py
+++ edgydata/constants.py
@@ -3,3 +3,18 @@ from enum import Enum
 POWER_TYPES = set(['self_consumed', 'generated', 'imported', 'consumed',
                    'exported'])
 POWER = Enum("power", " ".join(sorted(POWER_TYPES)))
+
+
+class DatePreset(Enum):
+    DAY = 1
+    WEEK = 2
+    MONTH = 3
+    YEAR = 4
+
+
+class Combiners(Enum):
+    SUM = 1
+    MEAN = 2
+    MIN = 3
+    MAX = 4
+    SPECIFIC = 5
